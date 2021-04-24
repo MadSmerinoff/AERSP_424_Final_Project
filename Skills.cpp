eae957735e84501648af0889f307c2e54a5d7390
Robert Fidler <42820691+MadSmerinoff@users.noreply.github.com>
2021-04-24 16:19:59 -0400

    Create Skills.cpp

---
+++ Skills.cpp
@@ -0,0 +1,21 @@
+/*
+	Skills.cpp
+
+	Created on Apr 24, 2021
+		Author: Robert
+*/
+
+#include <iostream>
+#include "Skills.h"
+
+Skills::Skills() {
+
+}
+
+//Member Functions
+
+int Skills::Passive() {
+
+	std::cout << "This does not have a passive ability. \n";
+	return -1;
+}
