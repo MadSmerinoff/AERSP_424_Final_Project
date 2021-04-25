cdb7a78d46e15119c6d84036f61b67c788c530cf
Robert Fidler <42820691+MadSmerinoff@users.noreply.github.com>
2021-04-25 12:24:24 -0400

    Create CHA.h

---
+++ CHA.h
@@ -0,0 +1,49 @@
+/*
+	CHA.h
+
+	Created on: Apr 25, 2021
+		Author: Robert
+*/
+
+#ifndef CHA_H_
+#define CHA_H_
+
+#include "Skills.h"
+
+class CHA : public Skills {
+
+public:
+	CHA();
+
+	void SetSavingThrowCHA();
+
+	void setDeceptionProf(bool);
+	int getDeceptionMod();
+
+	void setIntimidationProf(bool);
+	int getIntimidationMod();
+
+	void setPerformanceProf(bool);
+	int getPerformanceMod();
+
+	void setPersuasionProf(bool);
+	int getPersuasionMod();
+
+private:
+	bool DeceptionProf = false;
+	int DeceptionMod = 0;
+
+	bool IntimidationProf = false;
+	int IntimidationMod = 0;
+
+	bool PerformanceProf = false;
+	int PerformanceMod = 0;
+
+	bool PersuasionProf = false;
+	int PersuasionMod = 0;
+
+	int CHASaveThrowMod = 0;
+
+};
+
+#endif // !CHA_H_
