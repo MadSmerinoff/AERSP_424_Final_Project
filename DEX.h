caf9cff5d347e75a9b61d1eeb1588ca8ff11dd58
Robert Fidler <42820691+MadSmerinoff@users.noreply.github.com>
2021-04-25 12:18:40 -0400

    Create DEX.h

---
+++ DEX.h
@@ -0,0 +1,43 @@
+/*
+	DEX.h
+
+	Created on: Apr 24, 2021
+		Author: Robert
+*/
+
+#ifndef DEX_H_
+#define DEX_H_
+
+#include "Skills.h"
+
+class DEX : public Skills, public Character, public Class, public Race {
+
+public:
+	DEX();
+
+	void SetSavingThrowDEX();
+
+	void setAcrobaticsProf(bool);
+	int getAcrobaticsMod();
+
+	void setSleightofHandProf(bool);
+	int getSleightofHandMod();
+
+	void setStealthProf(bool);
+	int getStealthMod();
+
+private:
+	bool AcrobaticsProf = false;
+	int AcrobaticsMod = 0;
+
+	bool SleightofHandProf = false;
+	int SleightofHandMod = 0;
+
+	bool StealthProf = false;
+	int StealthMod = 0;
+
+	int DEXSaveThrowMod = 0;
+
+};
+
+#endif // !DEX_H_
