3cdef095d2d976d62bfa3b7504703b2bc624ef89
Robert Fidler <42820691+MadSmerinoff@users.noreply.github.com>
2021-04-25 12:23:33 -0400

    Create WIS.h

---
+++ WIS.h
@@ -0,0 +1,55 @@
+/*
+	WIS.h
+
+	Created on: Apr 25, 2021
+		Author: Robert
+*/
+
+#ifndef WIS_H_
+#define WIS_H_
+
+#include "Skills.h"
+
+class WIS : public Skills {
+
+public:
+	WIS();
+
+	void SetSavingThrowWIS();
+
+	void setAnimalHandlingProf(bool);
+	int getAnimalHandlingMod();
+
+	void setInsightProf(bool);
+	int getInsightMod();
+
+	void setMedicineProf(bool);
+	int getMedicineMod();
+
+	void setPerceptionProf(bool);
+	int getPerceptionMod();
+
+	void setSurvivalProf(bool);
+	int getSurvivalMod();
+
+private:
+	bool AnimalHandlingProf = false;
+	int AnimalHandlingMod = 0;
+
+	bool InsightProf = false;
+	int InsightMod = 0;
+
+	bool MedicineProf = false;
+	int MedicineMod = 0;
+
+	bool PerceptionProf = false;
+	int PerceptionMod = 0;
+
+	bool SurvivalProf = false;
+	int SurvivalMod = 0;
+
+	int WISSaveThrowMod = 0;
+
+};
+
+#endif // !WIS_H_
